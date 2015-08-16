40fc51208c2a4574174c8d8f720f85786690d895
RobinChen <chienpinchen@gmail.com>
2015-08-17 01:39:24 +0800

    Update Codes of Chapter 6

---
+++ Ch6/tour2.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct island
+{
+	char *name;
+	char *opens;
+	char *closes;
+	struct island *next;
+} island;
+
+void display(island *start)
+{
+	island *i = start;
+	for(; i != NULL; i= i->next)
+	{
+		printf("Name: %s open: %s-%s\n", i->name, i->opens, i->closes);
+	}
+}
+
+island* create(char *name)
+{
+	island *i = malloc(sizeof(island));
+	i->name = strdup(name);
+	i->opens = "09:00";
+	i->closes = "17:00";
+	i->next = NULL;
+	return i;
+}
+
+void release(island *start)
+{
+	island *i = start;
+	island *next = NULL;
+	for (; i != NULL; i = next)
+	{
+		next = i->next;
+		free(i->name);	// free memory of i->name first
+		free(i);		// then free memory of i 	
+	}
+}
+
+int main()
+{
+	island *start = NULL;
+	island *i = NULL;
+	island *next = NULL;
+	char name[80];
+	
+	for(; fgets( name, 80, stdin) != NULL; i = next)
+	{
+		next = create(name);
+		if(start == NULL)
+			start = next;
+		if(i != NULL)
+			i->next = next;
+	}
+	
+	display(start);
+	release(start);
+	//getchar();	// just for pause
+
+	return 0;
+}
